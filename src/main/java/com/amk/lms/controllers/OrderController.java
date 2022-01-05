8b50b206283b851e6e68be95347025b8cefb143e
chanthoeurn toun <chanthoeurn.toun@amkcambodia.com>
2022-01-05 17:33:36 +0700

    nothing

--- src/main/java/com/amk/lms/controllers/OrderController.java
+++ src/main/java/com/amk/lms/controllers/OrderController.java
@@ -2,16 +2,17 @@ package com.amk.lms.controllers;
 
 import com.amk.lms.exceptions.EtAuthException;
 import com.amk.lms.exceptions.EtInternalServerErrorException;
+import com.amk.lms.exceptions.EtResourceNotFoundException;
 import com.amk.lms.models.req.orders.OrderReq;
+import com.amk.lms.models.res.customers.CustomerRes;
 import com.amk.lms.models.res.orders.OrderRes;
 import com.amk.lms.services.impl.OrderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("order/v1/")
@@ -29,6 +30,16 @@ public class OrderController {
             return new ResponseEntity<OrderRes>(order, HttpStatus.OK);
         } catch (Exception ex) {
             throw new EtInternalServerErrorException(ex.getMessage().toString());
+        }
+    }
+    @GetMapping("orders")
+    public ResponseEntity<List<OrderRes>> GetOrderList() throws EtAuthException{
+        try{
+            List<OrderRes> resList = orderService.findAll();
+            return new ResponseEntity<List<OrderRes>>(resList,HttpStatus.OK);
+
+        }catch (Exception ex){
+            throw new EtResourceNotFoundException(ex.getMessage().toString());
         }
     }
 }
--- src/main/java/com/amk/lms/services/impl/OrderServiceImpl.java
+++ src/main/java/com/amk/lms/services/impl/OrderServiceImpl.java
@@ -4,9 +4,12 @@ import com.amk.lms.Repositories.OrderDetailRepository;
 import com.amk.lms.Repositories.OrderRepository;
 import com.amk.lms.exceptions.EtAuthException;
 import com.amk.lms.exceptions.EtInternalServerErrorException;
+import com.amk.lms.models.entities.Customer;
 import com.amk.lms.models.entities.Order;
 import com.amk.lms.models.entities.OrderDetail;
 import com.amk.lms.models.req.orders.OrderReq;
+import com.amk.lms.models.res.customers.AccountRes;
+import com.amk.lms.models.res.customers.CustomerRes;
 import com.amk.lms.models.res.orders.OrderDetailRes;
 import com.amk.lms.models.res.orders.OrderRes;
 import com.amk.lms.services.OrderService;
@@ -26,7 +29,36 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderRes> findAll() throws EtInternalServerErrorException {
-        return null;
+        try{
+            List<OrderRes> orderResList=new ArrayList<>();
+            List<Order> orderList = this.orderRepository.findAll();
+            for (int i=0;i<=orderList.size()-1;i++){
+                OrderRes orderRes = new OrderRes();
+                orderRes.setOrderId(orderList.get(i).getOrderId());
+                orderRes.setOrderDate(orderList.get(i).getOrderDate());
+                orderRes.setCustomerId(orderList.get(i).getCustomerId());
+                orderRes.setShipName(orderList.get(i).getShipName());
+                orderRes.setShippedDate(orderList.get(i).getShippedDate());
+                orderRes.setShipAddress(orderList.get(i).getShipAddress());
+
+                if(orderResList.get(i).getOrderDetailResList().size()>0){
+                    List<OrderDetailRes> orderDeResList = new ArrayList<>();
+                    for (int j=0;j<=orderList.get(i).getOrderDetailList().size()-1;j++){
+                        OrderRes orderDeRes = new OrderRes();
+                        accountRes.setAcctNo(customerList.get(i).getAccountList().get(j).getAcctNo());
+                        accountRes.setAcctCcy(customerList.get(i).getAccountList().get(j).getAcctCcy());
+                        accountRes.setAmount(customerList.get(i).getAccountList().get(j).getAmount());
+                        accountRes.setId(customerList.get(i).getAccountList().get(j).getId());
+                        accountResList.add(accountRes);
+                    }
+                    custRes.setAccountResList(accountResList);
+                }
+                customerResList.add(custRes);
+            }
+            return customerResList;
+        }catch (Exception ex){
+            throw new EtAuthException(ex.getMessage());
+        }
     }
 
     @Override
